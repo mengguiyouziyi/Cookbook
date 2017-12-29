26647c1ec7668e0d6cadf56ece97ecd4e63bf339
mengguiyouziyi <775618369@qq.com>
2017-12-29 11:19:17 +0800

    meishij_v1

--- cookbook/cookbook/spiders/meishijie.py
+++ cookbook/cookbook/spiders/meishijie.py
@@ -92,7 +92,9 @@ class MeishijieSpider(CrawlSpider):
         v_small = s.xpath('//a[@class="v_small"]/@title').extract_first()
         v_small = v_small if v_small else ''
         info = s.xpath('//div[@class="user"]/div[@class="info"]')
+        print(info.extract())
         span = info.xpath('./span/text()').extract_first()
+        print(span)
         cp_num = re.search(r'菜谱：(\d+)', span)
         cp_num = cp_num.group(1) if cp_num else ''
         gz_num = re.search(r'关注：(\d+)', span)
